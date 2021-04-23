de16521747f5ad2fa24b81284d8c64b223a4750a
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-04-23 16:12:11 -0700

    Add files via upload

--- 2gamers.js
+++ 2gamers.js
@@ -12,3 +12,17 @@ document.getElementById("player@2score").innerHTML = player2score;
 document.getElementById("questionturn").innerHTML = "questionturn " + playername1;
 document.getElementById("answerturn").innerHTML ="answerturn " + playername2;
 
+function send() {
+    num1 = document.getElementById("inputnumber1").value;
+    num2 = document.getElementById("inputnumber2").value;
+
+    realans = parseInt(num1) * parseInt(num2);
+    console.log(realans);
+
+    questionnum= "<h4>" + num1 + " x " + num2 +"</h4>";
+    inputbx = "<br> Your Answer: <input type='text' id='inputcheckbx";
+    chkbtn = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    all = questionnum + inputbx + chkbtn;
+
+    document.getElementById("output").innerHTML = all;
+}
